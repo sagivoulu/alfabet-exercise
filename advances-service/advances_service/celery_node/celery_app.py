85989b4487f01ba3aeec889821c41e98ce7b76fa
Sagiv Oulu <sagiv.oulu@gmail.com>
2023-06-14 00:15:05 +0300

    added scheduled call to celery task

--- advances-service/advances_service/celery_node/celery_app.py
+++ advances-service/advances_service/celery_node/celery_app.py
@@ -7,9 +7,15 @@ settings = Settings()
 celery_app = Celery('advance_service_node', broker=settings.redis_url, backend=settings.redis_url)
 
 
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(60, find_due_advance_payments.s(), name='find_due_advance_payments every 60 seconds')
+
+
 @celery_app.task
 def find_due_advance_payments():
     # TODO: Get all advance payments where their due date has passed & are still marked as not_due_yet
+    #           (order by advance & payment number)
     #       for each of these payments
     #           call the process_due_payment task on it & don't wait for the result
     #           change the payments status to pending_processing
